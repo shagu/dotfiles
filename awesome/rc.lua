44ed8027952097dd185036625370170a0ddb99a4
shagu <ericmauser@googlemail.com>
2021-05-14 13:53:13 +0200

    awesome: disable tag switch on desktop scroll

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -404,9 +404,9 @@ root.keys(awful.util.table.join(globalkeys,
 -- set root mouse binds
 root.buttons(awful.util.table.join(
   awful.button({ }, 1, function () mymainmenu:hide() end),
-  awful.button({ }, 3, function () mymainmenu:toggle() end),
-  awful.button({ }, 4, awful.tag.viewnext),
-  awful.button({ }, 5, awful.tag.viewprev)
+  awful.button({ }, 3, function () mymainmenu:toggle() end)
+  --awful.button({ }, 4, awful.tag.viewnext),
+  --awful.button({ }, 5, awful.tag.viewprev)
 ))
 
 -- define client keys
